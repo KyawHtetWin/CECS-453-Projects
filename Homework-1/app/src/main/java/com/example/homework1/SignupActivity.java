25a8734c7655f5e899f8f9b2a102130dc6e2a0df
Ben <bao.do777@gmail.com>
2020-02-19 15:30:15 -0800

    add comments

--- Homework-1/app/src/main/java/com/example/homework1/SignupActivity.java
+++ Homework-1/app/src/main/java/com/example/homework1/SignupActivity.java
@@ -9,28 +9,29 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.HashMap;
-
-
 
 public class SignupActivity extends AppCompatActivity {
 
+
+    // declare views
     private EditText usernameEditText;
     private EditText passwordEditText;
     private EditText retypePasswordEditText;
     private EditText emailEditText;
     private EditText phoneEditText;
-    private HashMap<String, String> credentials;
+
+    private HashMap<String, String> credentials; // hold usernames and passwords
 
 
-
+    // set up the activity components
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // initialize views
         usernameEditText = (EditText) findViewById(R.id.signup_editText_username);
         passwordEditText = (EditText) findViewById(R.id.signup_editText_password);
         retypePasswordEditText = (EditText) findViewById(R.id.signup_editText_retypePassword);
@@ -39,6 +40,7 @@ public class SignupActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
+        // retrieve credentials if one is passed in, else create new empty credentials
         if (bundle != null)
             credentials = (HashMap<String, String>) bundle.getSerializable("credentials");
         else {
@@ -46,14 +48,15 @@ public class SignupActivity extends AppCompatActivity {
             Toast.makeText(this, "Bundle is null", Toast.LENGTH_SHORT).show();
         }
 
-        
+        // OnFocusChange listener for the editText views
         View.OnFocusChangeListener editTextListener = new View.OnFocusChangeListener() {
 
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
 
                 switch (v.getId()) {
-
+                    // triggered by the username editText view
+                    // shows error for empty or non alphanumeric usernames
                     case R.id.signup_editText_username:
 
                         String username = usernameEditText.getText().toString().toLowerCase();
@@ -70,6 +73,8 @@ public class SignupActivity extends AppCompatActivity {
                         }
                         break;
 
+                    // triggered by the password editText view
+                    // shows error if password is empty or both passwords do not match
                     case R.id.signup_editText_password:
 
                         String password = passwordEditText.getText().toString();
@@ -87,6 +92,8 @@ public class SignupActivity extends AppCompatActivity {
                         }
                         break;
 
+                    // triggered by the retypePassword editText view
+                    // shows error if second password empty or both passwords do match
                     case R.id.signup_editText_retypePassword:
 
                         password = passwordEditText.getText().toString();
@@ -102,6 +109,8 @@ public class SignupActivity extends AppCompatActivity {
                         }
                         break;
 
+                    // triggered by the email editText view
+                    // shows error if email is empty or is not in correct format
                     case R.id.signup_editText_email:
 
                         String email = emailEditText.getText().toString();
@@ -116,6 +125,8 @@ public class SignupActivity extends AppCompatActivity {
                         }
                         break;
 
+                    // triggered by the phone editText view
+                    // shows error if phone number is empty or not in correct format
                     case R.id.signup_editText_phone:
                         String phoneNumber = phoneEditText.getText().toString();
 
@@ -132,6 +143,7 @@ public class SignupActivity extends AppCompatActivity {
             }
         };
 
+        // add listener to views
         usernameEditText.setOnFocusChangeListener(editTextListener);
         passwordEditText.setOnFocusChangeListener(editTextListener);
         retypePasswordEditText.setOnFocusChangeListener(editTextListener);
@@ -140,16 +152,21 @@ public class SignupActivity extends AppCompatActivity {
 
     }
 
+    // onclick method for the sign me up button
+    // create new user if data entered is valid, then starts login activity
     public void signMeUpButtonPressed(View v) {
 
+        // check if user entered data is valid before creating new user
         if (formIsValid()) {
 
             String username = usernameEditText.getText().toString().toLowerCase();
             String password = passwordEditText.getText().toString();
 
+            // add new user
             credentials.put(username, password);
             Toast.makeText(this, "Sign up successful!", Toast.LENGTH_SHORT).show();
 
+            // start login activity and pass in the new list of credentials
             Bundle bundle = new Bundle();
             Intent intent = new Intent(this, LoginActivity.class);
             bundle.putSerializable("credentials", credentials);
@@ -162,11 +179,13 @@ public class SignupActivity extends AppCompatActivity {
 
     }
 
+    // verify all entered data is valid to create new user
     public boolean formIsValid() {
 
         boolean hasNoError = true;
         boolean formNotEmpty = true;
 
+        // clear focus to trigger listener to check for errors
         usernameEditText.clearFocus();
         passwordEditText.clearFocus();
         retypePasswordEditText.clearFocus();
@@ -186,6 +205,7 @@ public class SignupActivity extends AppCompatActivity {
         String email = emailEditText.getText().toString();
         String phone = phoneEditText.getText().toString();
 
+        // check if editText views are empty
         if (username.isEmpty()) {
             usernameEditText.setError("Username cannot be empty");
             formNotEmpty = false;
