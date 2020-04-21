2934c7d18bcdf4accdb4047d2c76fd55183ca61c
KyawHtetWin <51302372+KyawHtetWin@users.noreply.github.com>
2020-04-21 00:15:01 -0700

    First Draft GPS
    Added Stopwatch
    Fetch the current location of the user using GPS at every second and can show the velocity of the device.

---
+++ Final Project/app/src/main/java/com/example/runningmate/MainActivity.java
@@ -0,0 +1,277 @@
+package com.example.runningmate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int DEFAULT_INTERVAL = 4;
+    public static final int FAST_INTERVAL = 1;
+    private static final int PERMISSIONS_FINE_LOCATION = 111;
+    // UI elements
+    TextView tv_lat, tv_lon, tv_acc, tv_sensor, tv_speed, tv_distance;
+    ToggleButton tb_start_pause;
+    Button btn_finish;
+
+    // Indicates the number of seconds passed on StopWatch
+    private int seconds;
+
+    // Indicates whether the stopwatch is running (Users physically running)
+    private boolean running;
+
+    // Indicates whether the stopwatch was running before screen rotation (Users physically running)
+    private boolean wasRunning;
+
+    // Google's API for location services
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    // Stores the user's current location at all time
+    Location mCurrentLocation;
+
+    // LocationRequest used to configure all settings related to FusedLocationProviderClient
+    LocationRequest locationRequest;
+    LocationCallback locationCallBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get References to UI
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_acc = findViewById(R.id.tv_accuracy);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_distance = findViewById(R.id.tv_distance);
+        tb_start_pause = findViewById(R.id.tb_start_pause);
+        btn_finish = findViewById(R.id.btn_finish);
+
+        // Restore the variables appropriately
+        if(savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+
+        else{
+            seconds = 0;
+            running = false;
+            wasRunning = false;
+        }
+
+
+        // Set the properties of LocationRequest
+        locationRequest = new LocationRequest();
+
+        // Retrieve location data every 8 secs
+        locationRequest.setInterval(1000 * DEFAULT_INTERVAL);
+
+        // Retrieve location data every 5 secs on the fastest interval
+        locationRequest.setFastestInterval(1000 * FAST_INTERVAL);
+
+        // Wants to retrieve with highest accuracy (i.e Using GPS)
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Get the user's last known location or their current location when the app runs for
+        // the first time
+        updateGPS();
+
+        // event that is triggered whenever the update interval is met
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                for(Location location: locationResult.getLocations()){
+                    if(location != null){
+
+                        mCurrentLocation = location;
+                        updateGPSUI(mCurrentLocation);
+                    }
+                }
+            }
+        };
+
+
+        tb_start_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the stopwatch when the Start Run is pressed
+                if(tb_start_pause.isChecked()) {
+                    // Start running
+                    running = true;
+                }
+                // Pause the stopwatch when the Pause is pressed
+                else {
+                    // Pause the running
+                    running = false;
+                }
+            }
+        });
+
+        setStopWatch();
+
+    } // End of OnCreate
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // If the user was running before the activity gets destroyed for some reasons, they
+        // should be able to continue running afterwards
+        if(wasRunning)
+            running = true;
+
+        // Start Location Update
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack,
+                null);
+    }
+
+    // Save the necessary variables to survive Activity destruction
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("running", running);
+        outState.putBoolean("wasRunning", wasRunning);
+    }
+
+    // This method is called when the user have finished a running section
+    public void onClickFinishRun(View view) {
+        running = false;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // If the user activity gets paused for some reason in the middle of the run,
+        // set the wasRunning to be able to restore its state appropriately
+        wasRunning = running;
+        running = false;
+
+        // Stop Location Update
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    // This method will update the stopwatch every second
+    private void setStopWatch() {
+        final  TextView tv_timer = findViewById(R.id.tv_timer);
+        // Creating a reference to a new Handler
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600) / 60;
+                int secs = seconds%60;
+
+                // Format the time
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                tv_timer.setText(time);
+
+                if(running)
+                    seconds++;
+
+                // Keep posting the code to be run after a delay of 1 second
+                handler.postDelayed(this, 1000* 1);
+            }
+        });
+    }
+
+    // Update the GPS
+    private void updateGPS() {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+                MainActivity.this);
+
+        // Check to make sure that permission for location is granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            // User provided the permission. So, get the last know location.
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this,
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Update the GPS GUI
+                            if (location != null) {
+                                //Toast.makeText(getApplicationContext(),
+                                 //       "Fetched the last known location",Toast.LENGTH_SHORT).show();
+                                mCurrentLocation = location;
+                                updateGPSUI(mCurrentLocation);
+                            }
+                        }
+                    });
+        }
+
+        else {
+            // Permission not granted yet
+            // Our build version needs to be higher than 23
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+
+    // This  method gets triggered after permission has been granted. requestCode is the
+    // number assigned, permissions specify which permission was requested.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted. Do nothing and carry on
+                }
+
+                else{
+                    Toast.makeText(this, "This app requires permission to be granted" +
+                            "to work properly", Toast.LENGTH_SHORT).show();
+                    // Exit the program
+                    finish();
+                }
+                break;
+        }
+    }
+
+
+    // Update GPS related UI values with the location passed
+    private void updateGPSUI(Location location) {
+
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_acc.setText(String.valueOf(location.getAccuracy()));
+
+        if(location.hasSpeed())
+            tv_speed.setText(String.valueOf(location.getSpeed()) + " m/s");
+        else
+            tv_speed.setText("Speed not available");
+
+    }
+
+
+}
